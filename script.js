1fc872512df691f2417d08a965b0ffbfe8de2cd9
PROF ROMULO <rjlpena@gmail.com>
2025-06-09 12:31:47 +0000

    ok

--- script.js
+++ script.js
@@ -17,15 +17,48 @@ const perguntas = [
             },
         ]
     },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+            texto: "Trazer dignidade e expandir a visibilidade cultural  afro-americana.",
+            afirmacao: "O 2PAC foi fundamental para a cultura afro-americana. "
+            },
+            {
+            texto: "Alternativa2",
+            afirmacao: "afirmacao1"
+            },
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+            texto: "Trazer dignidade e expandir a visibilidade cultural  afro-americana.",
+            afirmacao: "O 2PAC foi fundamental para a cultura afro-americana. "
+            },
+            {
+            texto: "Alternativa2",
+            afirmacao: "afirmacao1"
+            },
+        ]
+    },
 
 ];
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta(){
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
     mostraAlternativa();
 }
 
@@ -33,8 +66,25 @@ function mostraAlternativa(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativa = document.createElement("button");
         botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click",()=>respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativa);
-    }
+    
+}
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao + " ";
+    atual++;
+    mostraPergunta();
+
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Conclusão...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+
 }
 
 mostraPergunta();
